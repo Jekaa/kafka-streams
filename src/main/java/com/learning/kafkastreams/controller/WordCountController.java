72b130eefdaa22d9cf16b9bbb8bb01048a4397eb
GordienkoEE <gordienko.e.evg@sberbank.ru>
2022-03-11 08:59:43 +0300

    init commit

---
+++ src/main/java/com/learning/kafkastreams/controller/WordCountController.java
@@ -0,0 +1,25 @@
+package com.learning.kafkastreams.controller;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WordCountController {
+
+    StreamsBuilderFactoryBean factoryBean;
+
+    @GetMapping("/count/{word}")
+    public Long getWordCount(@PathVariable String word) {
+        KafkaStreams kafkaStreams = factoryBean.getKafkaStreams();
+        ReadOnlyKeyValueStore<String, Long> counts = kafkaStreams.store(
+                StoreQueryParameters.fromNameAndType("counts", QueryableStoreTypes.keyValueStore())
+        );
+        return counts.get(word);
+    }
+}
---
+++ src/main/java/com/learning/kafkastreams/proc/WordCountProcessor.java
@@ -0,0 +1,30 @@
+package com.learning.kafkastreams.proc;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class WordCountProcessor {
+
+    private static final Serde<String> STRING_SERDE = Serdes.String();
+
+    @Autowired
+    void buildPipeline(StreamsBuilder streamsBuilder) {
+        KStream<String, String> messageStream = streamsBuilder
+                .stream("input-topic", Consumed.with(STRING_SERDE, STRING_SERDE));
+
+        KTable<String, Long> wordCounts = messageStream
+                .mapValues((ValueMapper<String, String>) String::toLowerCase)
+                .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .groupBy((key, word) -> word, Grouped.with(STRING_SERDE, STRING_SERDE))
+                .count();
+
+        wordCounts.toStream().to("output-topic");
+    }
+}
